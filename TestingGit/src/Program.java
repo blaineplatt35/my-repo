c139e094b9091ea31ceb3166451e473ba44b8ec3
Blaine <bplatt@stumail.fhtc.edu>
2016-08-26 09:23:33 -0500

    Change Program.java

--- TestingGit/src/Program.java
+++ TestingGit/src/Program.java
@@ -1,9 +1,11 @@
 
 public class Program {
-
+	public Scanner in = new Scanner(System.in);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		in.close();
+		
 	}
 
 }
